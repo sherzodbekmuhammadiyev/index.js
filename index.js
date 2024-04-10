af7e538c45ff14119aa6795b8b91656d72b9f6e4
sherzodbekmuhammadiyev <sherzodbekmuhammadiyev1997@gmail.com>
2024-04-11 03:54:37 +0500

    Index js faylini yukladim

---
+++ index.js
@@ -0,0 +1,4 @@
+const calculator = (a ,b ) => {
+    console.log(`${a} + ${b} = ${ a + b } \n ${a} - ${b} = ${ a - b }\n ${a} * ${b} = ${ a * b } \n ${a} / ${b} = ${ a / b } \n`);
+}
+calculator( 4 ,8)
